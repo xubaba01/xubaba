cd8ddb60926c9c7658f6abbbdcecb033e73d30a3
xubaba01 <74659781+xubaba01@users.noreply.github.com>
2020-11-23 11:46:28 +0800

    Update 112301.App.java
    随机坐标

--- 112301.App.java
+++ 112301.App.java
@@ -4,7 +4,7 @@ public class APP {
     public static void main(String[] args) {
         while (true){
         int x,y;
-        System.out.println("输入一个随机数：");
+        System.out.println("输入一个数：");
         Scanner scanner=new Scanner(System.in);
         x=scanner.nextInt();
         x=(int)(Math.random()*100);
