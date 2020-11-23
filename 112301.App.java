2d3c3055b19999f91c6a7ed54a89d5904d3aa192
xubaba01 <74659781+xubaba01@users.noreply.github.com>
2020-11-23 11:56:38 +0800

    Update 112301.App.java
    随机坐标

--- 112301.App.java
+++ 112301.App.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class APP {
     public static void main(String[] args) {
         while (true){
-        int x,y;
-        System.out.println("输入一个数：");
+        int x,y,z;
+        System.out.println("请输入1：");
         Scanner scanner=new Scanner(System.in);
-        x=scanner.nextInt();
+        z=scanner.nextInt();
+            if(z==1){
         x=(int)(Math.random()*100);
         y=(int)(Math.random()*100);
         System.out.println("坐标："+"("+x+","+y+")");
     }
+        else {
+            System.out.println("输入错误：");
+            }}
 }}
