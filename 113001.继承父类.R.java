a9067566724186682e0b7deceffcaa5e0d8fcf74
xubaba01 <74659781+xubaba01@users.noreply.github.com>
2020-11-30 11:45:20 +0800

    Create 113001.继承父类.R.java

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,15 @@
+
+public class R {
+static void hb() {
+	String boy_filePath="img/boy.gif";
+	String girl_filePath="img/girl.gif";
+	int[][]array2d={{1,2,3},{4,5,6}};
+	System.out.print(boy_filePath+"\n"+girl_filePath+"\n");
+	for(int i=0;i<array2d.length;i++){
+        for(int j=0;j<array2d.length+1;j++){
+            System.out.print(array2d[i][j]);
+        }
+    }
+
+}
+}
