2db9dc2481e8bd13be564324e72cee115bc1421b
xubaba01 <74659781+xubaba01@users.noreply.github.com>
2020-12-07 21:37:19 +0800

    Update 120701.主类.APP.java

--- 120701.主类.APP.java
+++ 120701.主类.APP.java
@@ -1,11 +1,18 @@
-public class Xu {
-    public static void main(String[] args) {
-        int [][] array={
-                {1,2,3,4,5,6},
-                {7,8,9,10,0,12},
-                {8,8,9,10,11,12},
-                {7,8,3,10,11,12},
-                {88,66,92,102,151,999}
-        };
-        arrayFindValue(array,0);
-    }
+package Xu;
+
+public class D1 {
+
+@SuppressWarnings("unused")
+private static void arrayFindValue(int[][] array, int i) {
+	// TODO Auto-generated method stub
+	
+	int [][] array1={
+            {1,2,3,4,5,6},
+            {7,8,9,10,0,12},
+            {8,8,9,10,11,12},
+            {7,8,3,10,11,12},
+            {88,66,92,102,151,999}
+    };
+    arrayFindValue(array1,0);
+}
+}
